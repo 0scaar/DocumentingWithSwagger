7809adb5936cde2645839eaeb241b60d8451f8fe
Oscar <oscaar_2003@hotmail.com>
2020-03-21 11:13:10 -0300

    installing swashbuckle

--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -76,6 +76,17 @@ namespace Library.API
             services.AddScoped<IAuthorRepository, AuthorRepository>();
 
             services.AddAutoMapper();
+
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc(
+                    "LibraryOpenAPISpecification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Library API",
+                        Version = "1"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,6 +104,8 @@ namespace Library.API
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
 
             app.UseStaticFiles();
 
